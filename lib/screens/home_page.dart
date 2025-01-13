b8fdc6b338b9fe25dca3d810f8c9423a4ad2a2d6
ELDAROOS <eldarbolatbekuly2007@gmail.com>
2025-01-13 16:04:07 +0500

    ui update

---
+++ lib/screens/home_page.dart
@@ -0,0 +1,161 @@
+import 'package:bazarshop/screens/cart_page.dart';
+import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
+import 'auth_page.dart';
+import 'catalog_page.dart';
+import 'search_page.dart';
+
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  late VideoPlayerController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = VideoPlayerController.asset("assets/videoplayback.mp4")
+      ..initialize().then((_) {
+        _controller.setLooping(true);
+        _controller.play();
+        setState(() {});
+      });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  void navigateToSearch(String query) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => SearchPage(query: query),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          // Видео на заднем плане
+          if (_controller.value.isInitialized)
+            SizedBox.expand(
+              child: FittedBox(
+                fit: BoxFit.cover,
+                child: SizedBox(
+                  width: _controller.value.size.width,
+                  height: _controller.value.size.height,
+                  child: VideoPlayer(_controller),
+                ),
+              ),
+            ),
+          // Содержимое поверх видео
+          Column(
+            children: [
+              // Верхняя панель
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 20),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    // Кнопка каталог
+                    IconButton(
+                      icon: Icon(Icons.menu, color: Colors.white),
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => CatalogPage()),
+                        );
+                      },
+                    ),
+                    // Название BAZAR
+                    Text(
+                      "BAZAR",
+                      style: TextStyle(
+                        fontFamily: "Harper's Bazaar", // Укажи шрифт
+                        color: Colors.white,
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    // Кнопки лупа, профиль, корзина
+                    Row(
+                      children: [
+                        IconButton(
+                          icon: Icon(Icons.search, color: Colors.white),
+                          onPressed: () {
+                            navigateToSearch("");
+                          },
+                        ),
+                        IconButton(
+                          icon: Icon(Icons.person, color: Colors.black),
+                          onPressed: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(builder: (context) => AuthPage()),
+                            );
+                          },
+                        ),
+                        IconButton(
+                          icon: Icon(Icons.shopping_cart, color: Colors.white),
+                          onPressed: () {
+                            // Открыть корзину
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(builder: (context) => CartPage()),
+                            );
+                          },
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+              Spacer(),
+              // Нижняя строка поиска
+              Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: TextField(
+                        onSubmitted: navigateToSearch,
+                        style: TextStyle(color: Colors.white),
+                        decoration: InputDecoration(
+                          filled: true,
+                          fillColor: Colors.black.withOpacity(0.5),
+                          hintText: "Search...",
+                          hintStyle: TextStyle(color: Colors.white70),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(30),
+                            borderSide: BorderSide.none,
+                          ),
+                          contentPadding: EdgeInsets.symmetric(horizontal: 20),
+                        ),
+                      ),
+                    ),
+                    SizedBox(width: 10),
+                    IconButton(
+                      icon: Icon(Icons.search, color: Colors.white),
+                      onPressed: () {
+                        // Поиск
+                      },
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
